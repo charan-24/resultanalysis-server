112502f2112656a827e7e655e2ec8863fbc99170
charan-24 <charanpasupunooti137@gmail.com>
2024-04-13 19:10:52 +0530

    dbenv added

--- config/dbConn.js
+++ config/dbConn.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
-// const uri = "mongodb+srv://charanpasupunooti137:m8F3sXI29OEMjvNS@cluster0.komulec.mongodb.net/?retryWrites=true&w=majority";
-const uri = "mongodb://localhost:27017/resultanlaysis";
+const mongouri = process.env.MONGO_URI;
+const mongopwd = process.env.MONGO_PWD;
+const uri = `mongodb+srv://${mongouri}:${mongopwd}@cluster0.komulec.mongodb.net/?retryWrites=true&w=majority`;
+// const uri = "mongodb://localhost:27017/resultanlaysis";
 
 const connectDB = async () => {
    await mongoose.connect(uri)
